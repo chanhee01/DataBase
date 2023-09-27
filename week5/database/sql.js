e10b68eefbc86a9795e97e814db15740d50c46bd
chanhee <102280807+chanhee01@users.noreply.github.com>
2023-09-27 11:44:25 +0900

    5주자 실습

---
+++ week5/database/sql.js
@@ -0,0 +1,22 @@
+import mysql from 'mysql2';
+
+require("dotenv").config();
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '111111',
+    database: 'week4',
+});
+
+const promisePool = pool.promise();
+
+export const ApplyQuery = {
+    applyquery: async (Query) => {
+        const sql = Query;
+        const [result] = await promisePool.query(sql);
+
+        return result;
+    },
+};
